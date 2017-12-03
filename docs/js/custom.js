f42615f6df3770bf3a2a0e359de1566e2b093e65
Digender <mdigender@gmail.com>
2017-12-03 12:01:37 +0530

    Minified css and js

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,17 +1,1 @@
-$("#bars").click(function() {
-    if ($(this).hasClass('closed')) {
-        $(this).removeClass('closed').addClass('open').animate({ left: "280px" }, 400);
-        $('#drawer').show().animate({ left: "0" }, 400);
-        $(this).html('&times');
-    } else {
-        $(this).removeClass('open').addClass('closed').animate({ left: "0" }, 400);
-        $('#drawer').animate({ left: "-280px" }, 400);
-        $(this).html('&#9776;');
-    }
-});
-
-$('body').click(function(event) {
-    if ($(event.target).is('div#bars')) return;
-    $('#drawer').animate({ left: "-280px" }, 400);
-    $('#bars').addClass('closed').animate({ left: "0" }, 400).html('&#9776;');
-});
+$("#bars").click(function(){$(this).hasClass("closed")?($(this).removeClass("closed").addClass("open").animate({left:"280px"},400),$("#drawer").show().animate({left:"0"},400),$(this).html("&times")):($(this).removeClass("open").addClass("closed").animate({left:"0"},400),$("#drawer").animate({left:"-280px"},400),$(this).html("&#9776;"))}),$("body").click(function(a){$(a.target).is("div#bars")||($("#drawer").animate({left:"-280px"},400),$("#bars").addClass("closed").animate({left:"0"},400).html("&#9776;"))});
